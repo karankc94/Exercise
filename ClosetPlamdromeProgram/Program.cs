022e3bc1815d96ac422e62012b3eaf269d72f61a
karankc94 <karankc1994@gmail.com>
2018-05-29 14:23:35 +0530

    excercise book
    excercise notes

---
+++ ClosetPlamdromeProgram/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ClosetPlamdromeProgram
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    int i = 1;
+        //    int j = 0;
+        //    string input;
+        //    int.TryParse(Console.ReadLine(), out j);
+        //    do
+        //    {
+        //        input = Console.ReadLine();
+        //        Console.WriteLine(new Palindrome().ClosestPalindrom(input));
+        //        i++;
+        //    } while (i <= j);
+        //    Console.ReadLine();
+
+        //}
+
+    }
+    public class Palindrome
+    {
+        public string ClosestPalindrom(string n)
+        {
+            int order = (int)Math.Pow(10, n.Length / 2);
+            long original = long.Parse(n);
+            long mirrored = Mirror(original);
+
+            long larger = Mirror((original / order) * order + order + 1);
+            long smaller = Mirror((original / order) * order - 1);
+
+            if (mirrored >= original)
+            {
+                larger = Math.Min(mirrored, larger);
+            }
+            else if (mirrored < original)
+            {
+                smaller = Math.Max(mirrored, smaller);
+            }
+
+            return Convert.ToString(original - smaller <= larger - original ? smaller : larger);
+        }
+
+        public long Mirror(long pStr)
+        {
+            char[] charArray = pStr.ToString().ToCharArray();
+            int start = 0;
+            int end = charArray.Length - 1;
+            while (start < end)
+            {
+                charArray[end--] = charArray[start++];
+            }
+            return long.Parse(new string(charArray));
+        }
+    }
+}
---
+++ ClosetPlamdromeProgram/Sudoku.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+
+namespace ClosetPlamdromeProgram
+{
+    class Sudoku
+    {
+        public const int UNASSIGNED = 0;
+        int row = 0, col = 0;
+
+        /*
+* Function: SolveSudoku
+* ---------------------
+* Takes a partially filled-in grid and attempts to assign values to all
+* unassigned locations in such a way to meet the requirements for Sudoku
+* solution (non-duplication across rows, columns, and boxes). The function
+* operates via recursive backtracking: it finds an unassigned location with
+* the grid and then considers all digits from 1 to 9 in a loop. If a digit
+* is found that has no existing conflicts, tentatively assign it and recur
+* to attempt to fill in rest of grid. If this was successful, the puzzle is
+* solved. If not, unmake that decision and try again. If all digits have
+* been examined and none worked out, return false to backtrack to previous
+* decision point.
+*/
+        bool SolveSudoku(ref int[,] grid)
+        {
+           
+            if (!FindUnassignedLocation(ref grid, ref row, ref col)) return true; // success!
+            for (int num = 1; num <= 9; num++)
+            { // consider digits 1 to 9
+                if (NoConflicts(ref grid, row, col, num))
+                { // if looks promising,
+                    grid[row, col] = num; // make tentative assignment
+                    if (SolveSudoku(ref grid)) return true; // recur, if success, yay!
+                    grid[row, col] = UNASSIGNED; // failure, unmake & try again
+                }
+            }
+            return false; // this triggers backtracking
+        }
+        /*
+         * Function: FindUnassignedLocation
+         * --------------------------------
+         * Searches the grid to find an entry that is still unassigned. If found,
+         * the reference parameters row, col will be set the location that is
+         * unassigned, and true is returned. If no unassigned entries remain, false
+         * is returned.
+         */
+        bool FindUnassignedLocation(ref int[,] grid, ref int row, ref int col)
+        {
+            for (row = 0; row < grid.GetLength(0); row++)
+                for (col = 0; col < grid.GetLength(1); col++)
+                    if (grid[row, col] == UNASSIGNED) return true;
+            return false;
+        }
+        /*
+         * Function: NoConflicts
+         * ---------------------
+         * Returns a boolean which indicates whether it will be legal to assign
+         * num to the given row,col location. As assignment is legal if it that
+         * number is not already used in the row, col, or box.
+         */
+        bool NoConflicts(ref int[,] grid, int row, int col, int num)
+        {
+            return !UsedInRow(ref grid, row, num) && !UsedInCol(ref grid, col, num)
+            && !UsedInBox(ref grid, row - row % 3, col - col % 3, num);
+        }
+        /*
+         * Function: UsedInRow
+         * -------------------
+         * Returns a boolean which indicates whether any assigned entry
+         * in the specified row matches the given number.
+         */
+        bool UsedInRow(ref int[,] grid, int row, int num)
+        {
+            for (int col = 0; col < grid.GetLength(1); col++)
+                if (grid[row, col] == num) return true;
+            return false;
+        }
+        /*
+         * Function: UsedInCol
+         * -------------------
+         * Returns a boolean which indicates whether any assigned entry
+         * in the specified column matches the given number.
+         */
+        bool UsedInCol(ref int[,] grid, int col, int num)
+        {
+            for (int row = 0; row < grid.GetLength(0); row++)
+                if (grid[row, col] == num) return true;
+            return false;
+        }
+        /*
+         * Function: UsedInBox
+         * -------------------
+         * Returns a boolean which indicates whether any assigned entry
+         * within the specified 3x3 box matches the given number.
+         */
+        bool UsedInBox(ref int[,] grid, int boxStartRow, int boxStartCol, int num)
+        {
+            for (int row = 0; row < 3; row++)
+                for (int col = 0; col < 3; col++)
+                    if (grid[row + boxStartRow, col + boxStartCol] == num)
+                        return true;
+            return false;
+        }
+        public static void Main(string[] args)
+        {
+            // 0 means unassigned cells
+            int[,] grid = new int[9, 9]
+                   {{3, 0, 6, 5, 0, 8, 4, 0, 0},
+                    {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                    {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                    {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                    {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                    {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                    {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+
+            if (new Sudoku().SolveSudoku(ref grid) == true)
+                Console.WriteLine(grid);
+            //       printGrid(grid);
+            else
+            {
+                Console.WriteLine(grid);
+                foreach(var item in grid)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+         Console.WriteLine("No solution exists");
+            Console.ReadLine();
+           
+        }
+    }
+
+}
